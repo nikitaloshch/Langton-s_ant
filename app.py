0735b3b68094cf1418b7d47f45e458e81789c9fc
Никита Лощинин <terasentplay@yandex.ru>
2023-11-10 19:26:56 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+from PIL import Image
+
+
+def ant_path(size):
+    ant_pos = np.array([size // 2, size // 2])  # начальная позиция в центре поля
+    ant_direction = 0  # 0: вверх, 1: вправо, 2: вниз, 3: влево
+    directions = np.array([[-1, 0], [0, 1], [1, 0], [0, -1]])  # Изменения координат в зависимости от направления
+    image_data = np.ones((size, size), dtype=np.uint8) * 255  # белый фон
+    black_cell_count = 0  # Счетчик черных клеток
+
+    while all(0 <= pos < size for pos in ant_pos):
+        is_white_cell = image_data[tuple(ant_pos)] == 255
+        ant_direction = (ant_direction + 1) % 4 if is_white_cell else (ant_direction - 1) % 4
+        # Инвертирование пикселя
+        image_data[tuple(ant_pos)] ^= 255
+        black_cell_count += not is_white_cell
+        # Перемещение в соответствии с текущим направлением
+        ant_pos = (ant_pos + directions[ant_direction])
+
+    return image_data, black_cell_count
+
+
+def save_image(image_data, filename):
+    img = Image.fromarray(image_data, 'L')
+    img.save(filename, format='PNG')
+
+
+def main():
+    size = 1024
+
+    image_data, black_cell_count = ant_path(size)
+    save_image(image_data, 'ant_path.png')
+
+    print(f'Количество черных клеток: {black_cell_count}')
+
+
+if __name__ == '__main__':
+    main()
